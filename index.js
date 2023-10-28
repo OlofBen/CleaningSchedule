df6657f9c484d1627050cf7d51f52434e9ac345c
Olof Bengtsson <ol1058be-s@student.lu.se>
2023-10-28 11:57:33 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const hjalmar = document.getElementById('Hjalmar')
+const pontus = document.getElementById('Pontus')
+const olof = document.getElementById('Olof')
+
+const duty = ['Toa', 'Sopor', 'Golv']
+
+//code from https://www.geeksforgeeks.org/calculate-current-week-number-in-javascript/
+const currentDate = new Date();
+const startDate = new Date(currentDate.getFullYear(), 0, 1);
+const days = Math.floor((currentDate - startDate) /
+    (24 * 60 * 60 * 1000));
+ 
+const week = Math.ceil(days / 7);
+
+hjalmar.appendChild(createDuty(duty[week % 3]))
+pontus.appendChild(createDuty(duty[(week + 1) % 3]))
+olof.appendChild(createDuty(duty[(week + 2) % 3]))
+
+
+function createDuty(week, duty) {
+  const dutyElement = document.createElement('td')
+  dutyElement.innerHTML = week
+  return dutyElement
+}
